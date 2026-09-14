61d4df0a85abf0e7d5c1a59fe89715e6d5cee4a1
nilangaherath <nilangaherath7@gmail.com>
2026-09-14 23:14:40 +0530

    seat balance controller added

---
+++ src/main/java/com/examples/busreservation/repository/BookingRepository.java
@@ -0,0 +1,18 @@
+package com.example.busreservation.repository;
+
+import com.example.busreservation.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    
+    List<Booking> findByScheduleScheduleId(Integer scheduleId);
+    
+    List<Booking> findByEmail(String email);
+
+    long countByUserNic(String nic);
+}
+
